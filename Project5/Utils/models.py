079641ee2a349c2d050c76f2aa8f2c752e67eb43
Ramon Fernandes Viana <ramon.fv95@gmail.com>
2024-06-18 16:59:44 -0300

    feat: create a sequential LSTM

--- Project5/Utils/models.py
+++ Project5/Utils/models.py
@@ -3,29 +3,41 @@ import torch.nn as nn
 import numpy as np
 from matplotlib import pyplot as plt    
 
+# class LSTMModel(nn.Module):
+#     def __init__(self, inputFeatures, hiddenFeatures, layerDimension, outputDimension):
+#         super(LSTMModel, self).__init__()
+#         self.hiddenFeatures = hiddenFeatures
+#         self.layerDimension = layerDimension   
+#         self.lstm = nn.LSTM(inputFeatures, hiddenFeatures, layerDimension, batch_first=True)
+#         self.fc1 = nn.Linear(64, 64)
+#         self.fc2 = nn.Linear(64, outputDimension)
+
+#     def forward(self, x):
+#         x, (h, c) = self.lstm(x)
+#         x = x[:, -1, :] 
+#         x = self.fc1(x)
+#         x = nn.ReLU()(x)
+#         x = self.fc2(x)
+#         return x
+    
 class LSTMModel(nn.Module):
     def __init__(self, inputFeatures, hiddenFeatures, layerDimension, outputDimension):
         super(LSTMModel, self).__init__()
         self.hiddenFeatures = hiddenFeatures
         self.layerDimension = layerDimension   
         self.lstm = nn.LSTM(inputFeatures, hiddenFeatures, layerDimension, batch_first=True)
-        self.fc = nn.Linear(hiddenFeatures, outputDimension)
-
-    # def forward(self, x):
-    #     # h0 = torch.zeros(self.layerDimension, x.size(0), self.hiddenFeatures).requires_grad_()
-    #     # c0 = torch.zeros(self.layerDimension, x.size(0), self.hiddenFeatures).requires_grad_()
-    #     # out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
-    #     x, (h, c) = self.lstm(x)
-    #     x = self.fc(x)
-        
-    #     return x
-        
+        self.fc1 = nn.Linear(64, 64)
+        self.fc2 = nn.Linear(64, outputDimension)
 
     def forward(self, x):
         x, (h, c) = self.lstm(x)
-        x = x[:, -1, :]  # Select only the last output of the sequence
-        x = self.fc(x)
+        x = x[:, -1, :] 
+        x = self.fc1(x)
+        x = nn.ReLU()(x)
+        x = self.fc2(x)
         return x
+    
+    
 class GRUModel(nn.Module):
     def __init__(self, inputFeatures, hiddenFeatures, layerDimension, outputDimension):
         super(GRUModel, self).__init__()
@@ -42,19 +54,28 @@ class GRUModel(nn.Module):
 
 def prepareSequences(data, seqLength):
     xs, ys = [], []
-    for i in range(len(data) - seqLength):
-        x = data[i:i+seqLength]
-        y = data[i+seqLength]
+    for i in range(seqLength, len(data)):
+        x = data[i-seqLength:i,0]
+        y = data[i,0]
         xs.append(x)
         ys.append(y)
-    return np.array(xs), np.array(ys)
+    xs, ys = np.array(xs), np.array(ys)
+    xs = np.reshape(xs, (xs.shape[0], xs.shape[1], 1))
+    return xs, ys
 
 def trainModel(model, xTrain, yTrain, xVal, yVal, numEpochs, learningRate, batchSize):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
+    print( 'Device:', device)
+    model = model.to(device)  
+
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learningRate)
     
+    xTrain, yTrain = xTrain.to(device), yTrain.to(device) 
+    xVal, yVal = xVal.to(device), yVal.to(device) 
+
     trainDataset = torch.utils.data.TensorDataset(xTrain, yTrain)
-    trainLoader = torch.utils.data.DataLoader(dataset=trainDataset, batch_size=batchSize, shuffle=True)
+    trainLoader = torch.utils.data.DataLoader(dataset=trainDataset, batch_size=batchSize, shuffle=False)
     
     valDataset = torch.utils.data.TensorDataset(xVal, yVal)
     valLoader = torch.utils.data.DataLoader(dataset=valDataset, batch_size=batchSize, shuffle=False)
@@ -62,13 +83,13 @@ def trainModel(model, xTrain, yTrain, xVal, yVal, numEpochs, learningRate, batch
     for epoch in range(numEpochs):
         model.train()
         for i, (inputs, targets) in enumerate(trainLoader):
+            inputs, targets = inputs.to(device), targets.to(device)
             outputs = model(inputs)
             optimizer.zero_grad()
             loss = criterion(outputs, targets)
             loss.backward()
             optimizer.step()
         
-        # Validation
         model.eval()
         valLoss = 0.0
         with torch.no_grad():
--- Project5/weatherForecasting.py
+++ Project5/weatherForecasting.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import MinMaxScaler
 import torch
 import torch.nn as nn
 
+
+# print(torch.cuda.is_available())
+
 data = ReadData()
 
 df = data.loadDataset("Utils/temp_extracted/jena_climate_2009_2016.csv")
@@ -18,32 +21,33 @@ train, setTest = train_test_split(indexdData['T (degC)'].values, test_size=0.2, 
 validation, test = train_test_split(setTest, test_size=0.25, random_state=42, shuffle=False)
 
 scaler = MinMaxScaler(feature_range=(-1, 1))
-trainNormalized = scaler.fit_transform(train.reshape(-1, 1)).flatten()
-validationNormalized = scaler.transform(validation.reshape(-1, 1)).flatten()
-testNormalized = scaler.transform(test.reshape(-1, 1)).flatten()
+trainNormalized = scaler.fit_transform(train.reshape(-1, 1)).flatten().reshape(-1, 1)
+validationNormalized = scaler.transform(validation.reshape(-1, 1)).flatten().reshape(-1, 1)
+testNormalized = scaler.transform(test.reshape(-1, 1)).flatten().reshape(-1, 1)
 
-seq_length = 20  
+
+seq_length = 60 
 xTrain, yTrain = prepareSequences(trainNormalized, seq_length)
 xVal, yVal = prepareSequences(validationNormalized, seq_length)
 xTest, yTest = prepareSequences(testNormalized, seq_length)
 
-xTrain = torch.tensor(xTrain, dtype=torch.float32).unsqueeze(-1)
-yTrain = torch.tensor(yTrain, dtype=torch.float32).unsqueeze(-1)  # Ensure yTrain has the correct shape
-xVal = torch.tensor(xVal, dtype=torch.float32).unsqueeze(-1)
-yVal = torch.tensor(yVal, dtype=torch.float32).unsqueeze(-1)  # Ensure yVal has the correct shape
-xTest = torch.tensor(xTest, dtype=torch.float32).unsqueeze(-1)
-yTest = torch.tensor(yTest, dtype=torch.float32).unsqueeze(-1)  # Ensure yTest has the correct shape
+
+xTrain = torch.tensor(xTrain, dtype=torch.float32)
+yTrain = torch.tensor(yTrain, dtype=torch.float32).unsqueeze(-1)
+xVal = torch.tensor(xVal, dtype=torch.float32)
+yVal = torch.tensor(yVal, dtype=torch.float32).unsqueeze(-1)
+xTest = torch.tensor(xTest, dtype=torch.float32)
+yTest = torch.tensor(yTest, dtype=torch.float32).unsqueeze(-1)
 
 input_features = 1
-hidden_features = 50
-layer_dimension = 5
+hidden_features = 64
+layer_dimension = 3
 output_dimension = 1
-num_epochs = 50
+num_epochs = 10
 learning_rate = 0.001
-batch_size = 32  # Set an appropriate batch size
-
+batch_size = 32  
 model = LSTMModel(input_features, hidden_features, layer_dimension, output_dimension)
-
+print(f'Formato da entrada de treinamento: {xTrain.shape}')
 trainedModel = trainModel(model, xTrain, yTrain, xVal, yVal, num_epochs, learning_rate, batch_size)
 
 predicted = evaluateModel(trainedModel, xTest, yTest)
